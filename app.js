b92762898eb75fe9852157f4cb7087a3f3b7ec98
vojinovic.m1389@gmail.com <vojinovic.m1389@gmail.com>
2024-08-21 18:50:02 +0200

    Course examples

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+const peopleRouter = require("./routes/people");
+const authRouter = require("./routes/auth");
+
+// static assets
+app.use(express.static("./methods-public"));
+
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
+
+// routers
+app.use("/api/people", peopleRouter);
+app.use("/login", authRouter);
+
+// server listen
+app.listen(8001, () => {
+  console.log("Server is listening on port 8001...");
+});
